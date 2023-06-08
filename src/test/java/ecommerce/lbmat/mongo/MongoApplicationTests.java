5faeeb972dc51e7262cc38e2c7569b3c4cce820f
ErtKid <avrit.max@gmail.com>
2023-06-08 10:55:23 +0200

    Votre message de commit

--- src/test/java/ecommerce/lbmat/mongo/MongoApplicationTests.java
+++ src/test/java/ecommerce/lbmat/mongo/MongoApplicationTests.java
@@ -3,6 +3,7 @@ package ecommerce.lbmat.mongo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//testMAX
 @SpringBootTest
 class MongoApplicationTests {
 
