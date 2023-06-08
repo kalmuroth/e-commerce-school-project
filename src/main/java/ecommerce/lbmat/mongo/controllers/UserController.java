e64ed0b50d5c9ae96baa1717ee1e482e1bae7c09
ErtKid <avrit.max@gmail.com>
2023-06-08 13:57:43 +0200

    proprietes for serveur

--- src/main/java/ecommerce/lbmat/mongo/controllers/UserController.java
+++ src/main/java/ecommerce/lbmat/mongo/controllers/UserController.java
@@ -4,9 +4,9 @@ import ecommerce.lbmat.mongo.entities.User;
 import ecommerce.lbmat.mongo.services.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +15,8 @@ import java.util.List;
 @RequestMapping("/users")
 @Slf4j
 @AllArgsConstructor
+
+
 public class UserController {
     private UserService service;
 
@@ -26,6 +28,9 @@ public class UserController {
     public List<User> searchFor(@PathVariable String str){
         return service.findByStr(str);
     }
+
+
+    @CrossOrigin(origins = "http://localhost:54714") // replace with your Flutter web app's URL
     @GetMapping("/{id}")
     public User getAllFilms(@PathVariable String id){
         return service.read(id);
